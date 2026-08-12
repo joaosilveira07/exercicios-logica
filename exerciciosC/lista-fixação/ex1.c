f78f58031a020a5c709fd50afec08547ccb24283
João Pedro <joaopedrocva21@gmail.com>
2026-08-12 11:25:00 -0300

    adicionando lista de exercicios em C

---
+++ exerciciosC/lista-fixação/ex1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+// EXERCICIO 1
+// int main(){
+//     float n1, n2, media;
+
+//     printf("Digite a primeira nota: ");
+//     scanf("%f", &n1);
+//     printf("Digite a segunda nota: ");
+//     scanf("%f", &n2);
+
+//     media = ((n1 * 2) + (n2 * 3)) / 4;
+//     printf("Media = %.2f", media);
+// }
+
+// EXERCICIO 2
+// int main() {
+//     float salarioFixo, totalVendas, comissao, salarioFinal;
+
+//     printf("Digite o salario fixo: ");
+//     scanf("%f", &salarioFixo);
+
+//     printf("Digite o valor total das vendas: R$ ");
+//     scanf("%f", &totalVendas);
+
+//     comissao = totalVendas * 0.04;
+//     salarioFinal = salarioFixo + comissao;
+
+//     printf("\nComissao: R$ %.2f", comissao);
+//     printf("\nSalario Final: R$ %.2f\n", salarioFinal);
+
+//     return 0;
+// }
+
+// EXERCICIO 3
+// int main(){
+//     float peso, novoPeso;
+
+//     printf("Digite seu peso: ");
+//     scanf("%f", &peso);
+
+//     novoPeso += peso * 1.15;
+//     printf("Seu peso se vc engordar 15 porcento = %.2f %n", novoPeso);
+
+//     novoPeso += peso * 1.20;
+//     printf("Seu peso se vc engordar 20 porcento = %.2f %n", novoPeso);
+
+// }
+
+// EXERCICIO 4
+// int main(){
+//     float baseMaior, baseMenor, altura, area;
+
+//     printf("Digite a base maior: ");
+//     scanf("%f", &baseMaior);
+//     printf("Digite a base menor: ");
+//     scanf("%f", &baseMenor);
+//     printf("Agora digite a altura: ");
+//     scanf("%f", &altura);
+
+//     area = ((baseMaior + baseMenor) * altura) / 2;
+
+//     printf("Area = %.2f", area);
+// }
+
+// EXERCICIO 5
+// int main(){
+//     int anoNasc, anoAtual, idade;
+
+//     printf("Digite o ano em que voce nasceu: ");
+//     scanf("%d", &anoNasc);
+//     printf("Digite o ano atual: ");
+//     scanf("%d", &anoAtual);
+
+//     idade = anoAtual - anoNasc;
+//     printf("Voce tem %d anos.", idade);
+
+//     idade = idade * 12;
+//     printf("Voce tem %d meses.", idade);
+
+//     idade = idade * 30;
+//     printf("Voce tem %d dias.", idade);
+
+//     idade = idade / 4;
+//     printf("Voce tem %d semanas.", idade);
+// }
+
+// EXERCICIO 6
+int main(){
+    
+}
