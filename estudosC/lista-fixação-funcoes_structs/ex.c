af0e1264cb5d0b14d64d5be084f1f98dfcb173c1
João Pedro <joaopedrocva21@gmail.com>
2026-09-09 16:08:25 -0300

    ex 2
    Comment out the main function and related code for exercises.

--- estudosC/lista-fixação-funcoes_structs/ex.c
+++ estudosC/lista-fixação-funcoes_structs/ex.c
@@ -6,12 +6,35 @@
 #include <time.h>
 
 // EX 1
-int main(){
-    typedef struct {
-        char nome[30];
-        int idade;
-    } Pessoa;
+// int main(){
+//     typedef struct {
+//         char nome[30];
+//         int idade;
+//     } Pessoa;
 
-    Pessoa p = {"Marina", 21};
-    printf("%s tem %d anos.\n", p.nome, p.idade);
-}
+//     Pessoa p = {"Marina", 21};
+//     printf("%s tem %d anos.\n", p.nome, p.idade);
+// }
+
+// EX 2
+// typedef struct {
+//     char nome[30];
+//     char pais[30];
+//     float preco;
+// } Destino;
+
+// void imprimirDestino(Destino d){
+//     printf("Destino: %s\n", d.nome);
+//     printf("Pais: %s\n", d.pais);
+//     printf("Preco: %.2f\n", d.preco);
+// }
+
+// int main(){
+//     Destino d = {
+//         "Paris",
+//         "Franca",
+//         12500.99
+//     };
+//     imprimirDestino(d);
+
+// }
