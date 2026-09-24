ec8b0b4fae703f4e25f7561677dc56e291a37f9c
João Pedro <joaopedrocva21@gmail.com>
2026-09-24 14:51:47 -0300

    add lista de ponteiros com funcoes

---
+++ estudosC/lista-fixação-ponteiros-e-funcoes/ex.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+// EX 1
+// void inc_dec(int *a, int *b);
+
+// int main(){
+//     int a = 5;
+//     int b = 5;
+//     printf("Valores antes da funcao: A: %d, B: %d\n", a, b);
+//     inc_dec(&a, &b);
+//     printf("Valores depois da funcao: A: %d, B: %d\n", a, b);
+// }
+
+// void inc_dec(int *a, int *b){
+//     *a += 1;
+//     *b -= 1;
+// }
+
+// EX 2
+// void troca_valor(float *x, float *y);
+
+// int main(){
+//     float n1, n2;
+//     scanf("%f %f", &n1, &n2);
+//     printf("N1: %.1f, N2: %.1f\n", n1, n2);
+//     troca_valor(&n1, &n2);
+//     printf("N1: %.1f, N2: %.1f\n", n1, n2);
+//     return 0;
+// }
+
+// void troca_valor(float *x, float*y){
+//     float temp = *x;
+//     *x = *y;
+//     *y = temp;
+// }
+
+// EX 3
+// CONTINUAR AMANHÃ
